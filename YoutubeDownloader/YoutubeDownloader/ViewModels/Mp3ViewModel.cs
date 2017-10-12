828d54c5ac31b5d57a98ad599433ea15dc351457
Pawel Rudnicki <pawel.rudnicki@mobica.com>
2017-10-12 16:43:41 +0200

    Changed Mp3View.xaml a little bit

--- YoutubeDownloader/YoutubeDownloader/ViewModels/Mp3ViewModel.cs
+++ YoutubeDownloader/YoutubeDownloader/ViewModels/Mp3ViewModel.cs
@@ -1,12 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using System.Windows.Input;
 
 namespace YoutubeDownloader
 {
-    class Mp3ViewModel
+    class Mp3ViewModel : BaseViewModel
     {
+        #region Fields and Properties
+        private string _youtubeLinkUrl;
+        public string YoutubeLinkUrl
+        {
+            get
+            {
+                return _youtubeLinkUrl;
+            }
+            set
+            {
+                _youtubeLinkUrl = value;
+                OnPropertyChanged("YoutubeLinkUrl");
+            }
+        }
+        #endregion
+
+        #region Commands
+        public ICommand GoButtonCommand
+        {
+            get
+            {
+                return new RelayCommand(GoButtonClicked, CanExecute);
+            }
+        }
+        #endregion
+
+        #region Ctor
+        public Mp3ViewModel()
+        {
+            this.YoutubeLinkUrl = "Enter your link here!";
+        }
+        #endregion
+
+        #region Events
+        private void GoButtonClicked()
+        {
+            // TODO: write an validator for the string
+            if (YoutubeLinkUrl != string.Empty)
+            {
+                // TODO: perform action
+            }
+        }
+        #endregion
+
+        #region Methods
+        private bool CanExecute()
+        {
+            // TODO: some logic
+            return true;
+        }
+        #endregion
     }
 }
