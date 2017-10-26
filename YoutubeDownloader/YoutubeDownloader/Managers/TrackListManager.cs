c6a78299b0af2f8a4ea7d5eb88a032406554be04
Paweł Rudnicki <pawel.rudnicki@mobica.com>
2017-10-26 15:33:03 +0200

    Added percentage converter (tmp)

---
+++ YoutubeDownloader/YoutubeDownloader/Managers/TrackListManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YoutubeDownloader
+{
+    public sealed class TrackListManager
+    {
+        private static volatile TrackListManager _instance;
+        private static List<VideoModel> _trackList;
+        private static object syncRoot = new Object();
+
+        private TrackListManager() { }
+
+        public static TrackListManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new TrackListManager();
+                            _trackList = new List<VideoModel>();
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        public void UpdateList(VideoModel videoModel)
+        {
+            if (_trackList != null)
+            {
+                _trackList.Add(videoModel);
+            }
+        }
+
+        public List<VideoModel> GetItems()
+        {
+            if (_trackList != null && _trackList.Count > 0)
+            {
+                return _trackList;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ YoutubeDownloader/YoutubeDownloader/Utilities/ProgressBarConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace YoutubeDownloader
+{
+    [ValueConversion(typeof(String), typeof(String))]
+
+    public class ProgressBarConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                String str = value as String;
+                String strValue = str;
+                strValue += "%";
+                return strValue;
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
