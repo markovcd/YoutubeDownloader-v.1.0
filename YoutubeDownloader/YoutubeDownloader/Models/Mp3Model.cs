66cf3fc23b6672f052fe944e10db64005a1e0c07
Arkadiusz Markowski <arkadiusz.markowski@avid.com>
2018-02-14 14:13:47 +0100

    Preparing for Mp3ModelState converters to separate UI logic from data logic

--- YoutubeDownloader/YoutubeDownloader/Models/Mp3Model.cs
+++ YoutubeDownloader/YoutubeDownloader/Models/Mp3Model.cs
@@ -3,9 +3,40 @@ using System.Windows;
 
 namespace YoutubeDownloader
 {
+    public enum Mp3ModelState
+    {
+        None, Downloading, Converting, Done, Error
+    }
+
+    // TODO: obsłużyć Mp3ModelState i zrobić convertery do tego
+
     sealed public class Mp3Model : BindableBase
     {
         public string Name { get; set; }
+
+        private string _path;
+        public string Path
+        {
+            get { return _path; }
+            set { SetProperty(ref _path, value); }
+        }
+
+        public string FileName
+        {
+            get { return System.IO.Path.GetFileName(Path); }
+        }
+
+        public string FileNameWithoutExtension
+        {
+            get { return System.IO.Path.GetFileNameWithoutExtension(Path); }
+        }
+
+        private Mp3ModelState _state;
+        public Mp3ModelState State
+        {
+            get { return _state; }
+            set { SetProperty(ref _state, value); }
+        }
 
         private double _currentProgress;
         public double CurrentProgress
